089c490a91a158956c3f77ed8396021e2431347f
Douglas de Souza Zapelini <douglasszapelini@gmail.com>
2022-12-24 02:56:46 -0300

    Mapping keys from keyboard

--- src/app/calculator-content/calculator-content.component.ts
+++ src/app/calculator-content/calculator-content.component.ts
@@ -1,5 +1,5 @@
 import { OPERATIONS } from './../utils/util';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-calculator-content',
   templateUrl: './calculator-content.component.html',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 
 export class CalculatorContentComponent {
+  //Mapping keyboard events
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    this.keyboardEvents(event);
+  }
+
   actualNumber: string;
   maxDigits: number = 16;
   operation: string;
@@ -144,5 +150,72 @@ export class CalculatorContentComponent {
 
   parseStringToFloat(str: string): number {
     return Number.parseFloat(str.replaceAll('.', '').replace(',', '.'));
+  }
+
+  keyboardEvents(event: KeyboardEvent): void {
+    switch (event.key) {
+      case 'Escape':
+        this.clearNumber();
+        break;
+      case 'Backspace':
+        this.backspace();
+        break;
+      case 'Enter':
+        this.calculateResult(true);
+        break;
+      case OPERATIONS.add.symbol:
+        this.addOperation(OPERATIONS.add.symbol);
+        break;
+      case OPERATIONS.subtract.symbol:
+        this.addOperation(OPERATIONS.subtract.symbol);
+        break;
+      case '*':
+        this.addOperation(OPERATIONS.multiply.symbol);
+        break;
+      case '/':
+        this.addOperation(OPERATIONS.divide.symbol);
+        break;
+      case OPERATIONS.percentage.symbol:
+        this.addOperation(OPERATIONS.percentage.symbol);
+        break;
+      case '1':
+        this.addDigit('1');
+        break;
+      case '2':
+        this.addDigit('2');
+        break;
+      case '3':
+        this.addDigit('3');
+        break;
+      case '4':
+        this.addDigit('4');
+        break;
+      case '5':
+        this.addDigit('5');
+        break;
+      case '6':
+        this.addDigit('6');
+        break;
+      case '7':
+        this.addDigit('7');
+        break;
+      case '8':
+        this.addDigit('8');
+        break;
+      case '9':
+        this.addDigit('9');
+        break;
+      case '0':
+        this.addDigit('0');
+        break;
+      case ',':
+        this.addDigit(',');
+        break;
+      case '.':
+        this.addDigit(',');
+        break;
+      default:
+        break;
+    }
   }
 }
